1c882768cde9ff02f4ec9b3873020cbf3cbd0ae1
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2023-06-23 14:18:18 +0900

    #2 challenges css edit

--- js/weather.js
+++ js/weather.js
@@ -9,6 +9,7 @@ function onGeoOk(position) {
     .then((data) => {
       const weather = document.querySelector('#weather span:first-child');
       const city = document.querySelector('#weather span:last-child');
+      console.dir(data);
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
